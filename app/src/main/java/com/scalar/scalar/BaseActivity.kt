68854560c1fa606f51db2a277b91da059eb50199
Daniel Wang <danielw68@gmail.com>
2019-06-22 15:44:04 -0400

    Initial commit :fire:

---
+++ app/src/main/java/com/scalar/scalar/BaseActivity.kt
@@ -0,0 +1,9 @@
+package com.scalar.scalar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    // TODO: We will invoke Pin login if the the app ever went to onPaused
+}
---
+++ app/src/main/java/com/scalar/scalar/activities/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.scalar.scalar.activities
+
+import android.os.Bundle
+import com.scalar.scalar.BaseActivity
+import com.scalar.scalar.R
+
+class LoginActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
