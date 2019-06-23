3c62ccc8ab0d44e3dcea2c5dc4d39cdb61c969c9
Daniel Wang <danielw68@gmail.com>
2019-06-23 01:44:49 -0400

    Add Create account inital activity

---
+++ app/src/main/java/com/scalar/scalar/activities/CreateAccountActivity.kt
@@ -0,0 +1,13 @@
+package com.scalar.scalar.activities
+
+import android.os.Bundle
+import com.scalar.scalar.BaseActivity
+import com.scalar.scalar.R
+
+class CreateAccountActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
--- app/src/main/java/com/scalar/scalar/activities/LoginActivity.kt
+++ app/src/main/java/com/scalar/scalar/activities/LoginActivity.kt
@@ -1,13 +1,23 @@
 package com.scalar.scalar.activities
 
+import android.content.Intent
 import android.os.Bundle
 import com.scalar.scalar.BaseActivity
 import com.scalar.scalar.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        createWalletButton.setOnClickListener {
+            startActivity(Intent(this, CreateAccountActivity::class.java))
+        }
+
+        importWalletButton.setOnClickListener {
+            // TODO: Start ImportActivity
+        }
     }
 }
