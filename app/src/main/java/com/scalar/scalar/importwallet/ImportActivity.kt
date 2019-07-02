7873c9d41a5b34134ae4788a08d43b95bdb3e537
Daniel Wang <danielw68@gmail.com>
2019-07-01 23:37:15 -0400

    Add ImportActivity class, very tired so I'll actually populate it tomorrow

---
+++ app/src/main/java/com/scalar/scalar/importwallet/ImportActivity.kt
@@ -0,0 +1,13 @@
+package com.scalar.scalar.importwallet
+
+import android.os.Bundle
+import com.scalar.scalar.BaseActivity
+import com.scalar.scalar.R
+
+class ImportActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_import)
+    }
+}
