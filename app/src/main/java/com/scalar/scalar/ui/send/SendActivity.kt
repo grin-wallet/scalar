8b7a79a4f69babc0ad6daceeeb032fc0c14b052c
Daniel Wang <danielw68@gmail.com>
2019-07-16 21:17:46 -0400

    Add send screen

---
+++ app/src/main/java/com/scalar/scalar/ui/send/SendActivity.kt
@@ -0,0 +1,14 @@
+package com.scalar.scalar.ui.send
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.scalar.scalar.R
+import com.scalar.scalar.ui.activities.BaseActivity
+
+class SendActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send)
+    }
+}
