9f36c4467a607ac4b7eef8ea1e57b4226efa1c87
Daniel Wang <danielw68@gmail.com>
2019-07-17 23:46:17 -0400

    Add PinActivity and views

---
+++ app/src/main/java/com/scalar/scalar/ui/activities/PinActivity.kt
@@ -0,0 +1,13 @@
+package com.scalar.scalar.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.scalar.scalar.R
+
+class PinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pin)
+    }
+}
